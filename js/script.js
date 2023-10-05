52ca352f7da9ee67e69c87fab442113648cbb668
DanniAdhyatma <dannirachman2@gmail.com>
2023-10-05 17:10:08 +0800

    add validation code javascript for schedule page

--- js/script.js
+++ js/script.js
@@ -1,65 +1,106 @@
+// add content in navbar mobile device
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+  nav.classList.toggle('slide');
+});
+
 // start schedule page
 let boxes = document.querySelectorAll('.category-box');
-      let selectedCategory = document.getElementById('res-cat');
-      boxes.forEach(function (box) {
-        box.addEventListener('click', function () {
-          // Add "selected" class to the clicked category box
-          if (box.classList.contains('selected')) {
-            box.classList.remove('selected');
-            selectedCategory.textContent = 'None';
-          } else {
-            boxes.forEach(function (el) {
-              // Remove "selected" class from all category boxes
-              el.classList.remove('selected');
-            });
-            box.classList.add('selected');
-            selectedCategory.textContent = box.querySelector('p').textContent;
-          }
-        });
+let selectedCategory = document.getElementById('res-cat');
+boxes.forEach(function (box) {
+  box.addEventListener('click', function () {
+    // Add "selected" class to the clicked category box
+    if (box.classList.contains('selected')) {
+      box.classList.remove('selected');
+      selectedCategory.textContent = 'None';
+    } else {
+      boxes.forEach(function (el) {
+        // Remove "selected" class from all category boxes
+        el.classList.remove('selected');
       });
+      box.classList.add('selected');
+      selectedCategory.textContent = box.querySelector('p').textContent;
+    }
+  });
+});
 
-      let dates = document.querySelectorAll('.date-box');
-      let selectedDates = document.getElementById('res-date');
-      dates.forEach(function (date) {
-        date.addEventListener('click', function () {
-          // Add "selected" class to the clicked date box
-          if (date.classList.contains('selected')) {
-            date.classList.remove('selected');
-            selectedDates.textContent = 'None';
-          } else {
-            // Remove "selected" class from all dates boxes
-            dates.forEach(function (el) {
-              el.classList.remove('selected');
-            });
-            date.classList.add('selected');
-            selectedDates.textContent = date.querySelector('p').textContent;
-          }
-        });
+let dates = document.querySelectorAll('.date-box');
+let selectedDates = document.getElementById('res-date');
+dates.forEach(function (date) {
+  date.addEventListener('click', function () {
+    // Add "selected" class to the clicked date box
+    if (date.classList.contains('selected')) {
+      date.classList.remove('selected');
+      selectedDates.textContent = 'None';
+    } else {
+      // Remove "selected" class from all dates boxes
+      dates.forEach(function (el) {
+        el.classList.remove('selected');
       });
+      date.classList.add('selected');
+      selectedDates.textContent = date.querySelector('p').textContent;
+    }
+  });
+});
 
-      let times = document.querySelectorAll('.time-box');
-      let selectedTime = document.getElementById('res-time');
-      times.forEach(function (time) {
-        time.addEventListener('click', function () {
-          // Add "selected" class to the clicked time box
-          if (time.classList.contains('selected')) {
-            time.classList.remove('selected');
-            selectedTime.textContent = 'None';
-          } else {
-            // Remove "selected" class from all dates boxes
-            times.forEach(function (el) {
-              el.classList.remove('selected');
-            });
-            time.classList.add('selected');
-            selectedTime.textContent = time.querySelector('p').textContent;
-          }
-        });
+let times = document.querySelectorAll('.time-box');
+let selectedTime = document.getElementById('res-time');
+times.forEach(function (time) {
+  time.addEventListener('click', function () {
+    // Add "selected" class to the clicked time box
+    if (time.classList.contains('selected')) {
+      time.classList.remove('selected');
+      selectedTime.textContent = 'None';
+    } else {
+      // Remove "selected" class from all dates boxes
+      times.forEach(function (el) {
+        el.classList.remove('selected');
       });
+      time.classList.add('selected');
+      selectedTime.textContent = time.querySelector('p').textContent;
+    }
+  });
+});
 
-      let selectedDoctor = document.getElementById('res-doc');
-      selectedDoctor.textContent = document.querySelector('.doctorsName').textContent;
+let selectedDoctor = document.getElementById('res-doc');
+selectedDoctor.textContent = document.querySelector('.doctorsName').textContent;
 // end schedule page
 
+// add js validation for schedule page
+
+// Dapatkan elemen tombol submit
+const submitButton = document.querySelector("button[type='submit']");
+
+// Tambahkan event listener pada tombol submit
+submitButton.addEventListener('click', function (event) {
+  // Validasi category box
+  const selectedCategoryBox = document.querySelector('.category-box.selected');
+  if (selectedCategoryBox === null) {
+    // Category box tidak dipilih
+    event.preventDefault();
+    alert('Silahkan pilih kategori dokter');
+  }
+
+  // Validasi date box
+  const selectedDateBox = document.querySelector('.date-box.selected');
+  if (selectedDateBox === null) {
+    // Date box tidak dipilih
+    event.preventDefault();
+    alert('Silahkan pilih tanggal konsultasi');
+  }
+
+  // Validasi time box
+  const selectedTimeBox = document.querySelector('.time-box.selected');
+  if (selectedTimeBox === null) {
+    // Time box tidak dipilih
+    event.preventDefault();
+    alert('Silahkan pilih sesi konsultasi');
+  }
+});
+
+// end validation code
 
 // start patient-form page//
 document.addEventListener('DOMContentLoaded', function () {
@@ -148,11 +189,3 @@ function generatePDF() {
   html2pdf().from(element).save();
 }
 //end receipt page//
-
-// add content in navbar mobile device
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', function () {
-  nav.classList.toggle('slide');
-});
